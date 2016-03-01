85cfa427a4b2feb7e422c81b1ed8197979606ab0
wwd <ericrock@foxmail.com>
2016-03-01 10:52:34 +0800

    kernel: able to boot debian without ramdisk

--- burn_into_sd.sh
+++ burn_into_sd.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-boot_part=$(mount | grep /media/*/BOOT | awk '{print $3}')
+boot_part=$(mount | grep /media/*/BOOT* | awk '{print $3}')
 if [ -z $boot_part ] ; then
-    boot_part=$(mount | grep /media/*/boot | awk '{print $3}')
+    boot_part=$(mount | grep /media/*/boot* | awk '{print $3}')
 fi
 if [ -z $boot_part ] ; then
     echo "fail to find sdcard boot partition"
@@ -18,4 +18,4 @@ cp -fv tools/pack/out/sys_config.bin $boot_part/script.bin
 cd ./tools/pack > /dev/null
 ./fuse_uboot.sh /dev/${device}
 cd - > /dev/null
-eject /dev/sdg
+#eject /dev/${device}
