61c3caf5608ef99805687ff06c98a00d9a21a818
wuweidong <625769020@qq.com>
2018-12-03 16:24:01 +0800

    build: fa_tools: Fix bug when fusing u-boot by fastboot

--- fa_tools/fuse.sh
+++ fa_tools/fuse.sh
@@ -23,8 +23,9 @@ function execute_cmd()
 
 function usage()
 {
-    echo -e "\033[1;32mUsage: `basename $0` -d device[${H5_FUSE_DEVICE}] -p platform[linux] -t target[${H5_LINUX_TARGET}]\033[0m"
-    echo -e "\033[1;32mUsage: `basename $0` -d device[${H5_FUSE_DEVICE}] -p platform[android] -t target[${H5_ANDROID_TARGET}]\033[0m"
+    echo "Usage:"
+    echo -e "\t`basename $0` -d device[${FA_DEV}] -p platform[linux] -t target[${FA_LINUX_TARGET}]"
+    echo -e "\t`basename $0` -d device[${FA_DEV}] -p platform[android] -t target[${FA_ANDROID_TARGET}]"
     exit 1
 }
 
@@ -35,14 +36,15 @@ function parse_arg()
     fi
     OLD_IFS=${IFS}
     IFS="|"
+    local tmp
     while getopts "d:p:t:" opt
     do
         case $opt in
             d )
-                FUSE_DEVICE=$OPTARG
-                for TMP in ${H5_FUSE_DEVICE}
+                SELECTED_DEV=$OPTARG
+                for tmp in ${SELECTED_DEV}
                 do
-                    if [[ ${FUSE_DEVICE} = ${TMP} ]];then
+                    if [[ ${SELECTED_DEV} =~ ${tmp} ]];then
                         FOUND=1
                         break
                     else
@@ -56,10 +58,10 @@ function parse_arg()
                 check_fuse_device
                 ;;
             p )
-                PLATFORM=$OPTARG
-                for TMP in ${H5_PLATFORM}
+                SELECTED_PLATFORM=$OPTARG
+                for tmp in ${FA_PLATFORM}
                 do
-                    if [ ${PLATFORM} = ${TMP} ];then
+                    if [ ${SELECTED_PLATFORM} = ${tmp} ];then
                         FOUND=1
                         break
                     else
@@ -72,17 +74,18 @@ function parse_arg()
                 fi
                 ;;
             t )
-                TARGET=$OPTARG
-                if [ "x${PLATFORM}" = "xlinux" ]; then
-                    H5_TARGET=${H5_LINUX_TARGET}
-                elif [ "x${PLATFORM}" = "xandroid" ]; then
-                    H5_TARGET=${H5_ANDROID_TARGET}
+                local plat_target
+                SELECTED_TARGET=$OPTARG
+                if [ "x${SELECTED_PLATFORM}" = "xlinux" ]; then
+                    plat_target=${FA_LINUX_TARGET}
+                elif [ "x${SELECTED_PLATFORM}" = "xandroid" ]; then
+                    plat_target=${FA_ANDROID_TARGET}
                 else
                     usage
                 fi
-                for TMP in ${H5_TARGET}
+                for tmp in ${plat_target}
                 do
-                    if [ ${TARGET} = ${TMP} ];then
+                    if [ ${SELECTED_TARGET} = ${tmp} ];then
                         FOUND=1
                         break
                     else
@@ -103,8 +106,8 @@ function parse_arg()
 
 function check_fuse_device() 
 {
-    if [[ "x${FUSE_DEVICE}" =~ "x/dev/sd" ]]; then
-        DEV_NAME=`basename ${FUSE_DEVICE}`
+    if [[ "x${SELECTED_DEV}" =~ "x/dev/sd" ]]; then
+        DEV_NAME=`basename ${SELECTED_DEV}`
         BLOCK_CNT=`cat /sys/block/${DEV_NAME}/size`
         if [ $? -ne 0 ]; then
             pt_error "Can't find device ${DEV_NAME}"
@@ -120,7 +123,7 @@ function check_fuse_device()
             pt_error "Block device size (${BLOCK_CNT}) is too large"
             exit 1
         fi
-    elif [ "x${FUSE_DEVICE}" = "xfastboot" ]; then
+    elif [ "x${SELECTED_DEV}" = "xfastboot" ]; then
         fastboot devices | grep -q fastboot
         if [ $? -ne 0 ]; then
             pt_error "fastboot not ready"
@@ -133,21 +136,25 @@ function fuse_uboot()
 {
     BOOT0_FEX=${PRJ_ROOT_DIR}/tools/pack/out/boot0_sdcard.fex
     UBOOT_FEX=${PRJ_ROOT_DIR}/tools/pack/out/u-boot.fex
-    if [[ "x${FUSE_DEVICE}" =~ "x/dev/sd" ]]; then
-        execute_cmd "dd if=${BOOT0_FEX} of=${FUSE_DEVICE} bs=1k seek=8"
-        execute_cmd "dd if=${UBOOT_FEX} of=${FUSE_DEVICE} bs=1k seek=16400"
-    elif [ "x${FUSE_DEVICE}" = "xfastboot" ]; then
+    if [[ "x${SELECTED_DEV}" =~ "x/dev/sd" ]]; then
+        pt_info "fusing ${BOOT0_FEX}..."
+        execute_cmd "dd if=${BOOT0_FEX} of=${SELECTED_DEV} bs=1k seek=8"
+        pt_info "fusing ${UBOOT_FEX}..."
+        execute_cmd "dd if=${UBOOT_FEX} of=${SELECTED_DEV} bs=1k seek=16400"
+    elif [ "x${SELECTED_DEV}" = "xfastboot" ]; then
+        pt_info "fusing ${BOOT0_FEX}..."
         execute_cmd "fastboot flash boot0 ${BOOT0_FEX}"
-        execute_cmd "fastboot flash boot_package ${UBOOT_FEX}"
+        pt_info "fusing ${UBOOT_FEX}..."
+        execute_cmd "fastboot flash u-boot ${UBOOT_FEX}"
     fi
 }
 
 function fuse_env_fex_4_android()
 {
     ENV_FEX=${PRJ_ROOT_DIR}/tools/pack/out/env.fex
-    if [[ "x${FUSE_DEVICE}" =~ "x/dev/sd" ]]; then
-        execute_cmd "dd if=${ENV_FEX} of=${FUSE_DEVICE} bs=1M seek=52"
-    elif [ "x${FUSE_DEVICE}" = "xfastboot" ]; then
+    if [[ "x${SELECTED_DEV}" =~ "x/dev/sd" ]]; then
+        execute_cmd "dd if=${ENV_FEX} of=${SELECTED_DEV} bs=1M seek=52"
+    elif [ "x${SELECTED_DEV}" = "xfastboot" ]; then
         execute_cmd "fastboot flash env ${ENV_FEX}"
     fi    
 }
@@ -155,9 +162,9 @@ function fuse_env_fex_4_android()
 function fuse_boot_fex_4_android()
 {
     BOOT_FEX=${PRJ_ROOT_DIR}/tools/pack/out/boot.fex
-    if [[ "x${FUSE_DEVICE}" =~ "x/dev/sd" ]]; then
-        execute_cmd "dd if=${BOOT_FEX} of=${FUSE_DEVICE} bs=1M seek=68"
-    elif [ "x${FUSE_DEVICE}" = "xfastboot" ]; then
+    if [[ "x${SELECTED_DEV}" =~ "x/dev/sd" ]]; then
+        execute_cmd "dd if=${BOOT_FEX} of=${SELECTED_DEV} bs=1M seek=68"
+    elif [ "x${SELECTED_DEV}" = "xfastboot" ]; then
         execute_cmd "fastboot flash boot ${BOOT_FEX}"
     fi
 }
@@ -165,47 +172,54 @@ function fuse_boot_fex_4_android()
 function fuse_system_fex_4_android()
 {
     SYSTEM_FEX=${PRJ_ROOT_DIR}/tools/pack/out/system.fex
-    if [[ "x${FUSE_DEVICE}" =~ "x/dev/sd" ]]; then
-        execute_cmd "dd if=${SYSTEM_FEX} of=${FUSE_DEVICE} bs=1M seek=84"
-    elif [ "x${FUSE_DEVICE}" = "xfastboot" ]; then
+    if [[ "x${SELECTED_DEV}" =~ "x/dev/sd" ]]; then
+        execute_cmd "dd if=${SYSTEM_FEX} of=${SELECTED_DEV} bs=1M seek=84"
+    elif [ "x${SELECTED_DEV}" = "xfastboot" ]; then
         execute_cmd "fastboot flash system ${SYSTEM_FEX}"
     fi    
 }
 
 cd ..
 PRJ_ROOT_DIR=`pwd`
-H5_FUSE_DEVICE="/dev/sd|fastboot"
-H5_PLATFORM="linux|android"
-H5_ANDROID_TARGET="u-boot|env.fex|boot.fex|system.fex"
-H5_LINUX_TARGET="u-boot"
-PLATFORM=linux
-TARGET=u-boot
-FUSE_DEVICE=/dev/null
+
+LINUX_PLAT_MSG="Linux platform"
+ANDROID_PLAT_MSG="Android platform"
+
+# friendlyelec attribute
+FA_DEV="/dev/sd|fastboot"
+FA_PLATFORM="linux|android"
+FA_ANDROID_TARGET="u-boot|env.fex|boot.fex|system.fex"
+FA_LINUX_TARGET="u-boot"
+
+# user attribute
+SELECTED_PLATFORM=linux
+SELECTED_TARGET=u-boot
+SELECTED_DEV=/dev/null
 
 parse_arg $@
 
-if [ "x${PLATFORM}" = "xlinux" ]; then
-    if [ "x${TARGET}" = "xu-boot" ]; then
+if [ "x${SELECTED_PLATFORM}" = "xlinux" ]; then
+    if [ "x${SELECTED_TARGET}" = "xu-boot" ]; then
         fuse_uboot
-        pt_info "fuse u-boot for Linux platform success"
+        pt_info "fuse u-boot for ${LINUX_PLAT_MSG} success"
     else
         pt_error "unsupported target"
         usage
         exit 0
     fi
-elif [ "x${PLATFORM}" = "xandroid" ]; then
-    if [ "x${TARGET}" = "xu-boot" ]; then
+elif [ "x${SELECTED_PLATFORM}" = "xandroid" ]; then
+    if [ "x${SELECTED_TARGET}" = "xu-boot" ]; then
         fuse_uboot
-        pt_info "fuse u-boot for Android platform success"
-    elif [ "x${TARGET}" = "xenv.fex" ]; then
+        pt_info "fuse u-boot for ${ANDROID_PLAT_MSG} success"
+    elif [ "x${SELECTED_TARGET}" = "xenv.fex" ]; then
         fuse_env_fex_4_android
-        pt_info "fuse env.fex for Android platform success"
-    elif [ "x${TARGET}" = "xboot.fex" ]; then
+        pt_info "fuse env.fex for ${ANDROID_PLAT_MSG} success"
+    elif [ "x${SELECTED_TARGET}" = "xboot.fex" ]; then
         fuse_boot_fex_4_android
-        pt_info "fuse boot.fex for Android platform success"
-    elif [ "x${TARGET}" = "xsystem.fex" ]; then
+        pt_info "fuse boot.fex for ${ANDROID_PLAT_MSG} success"
+    elif [ "x${SELECTED_TARGET}" = "xsystem.fex" ]; then
         fuse_system_fex_4_android
-        pt_info "fuse system.fex for Android platform success"
+        pt_info "fuse system.fex for ${ANDROID_PLAT_MSG} success"
         exit 0
     else
         pt_error "unsupported target"
