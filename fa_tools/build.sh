69b4de2b0bd68513dce7de61767d73168f70d50a
wuweidong <625769020@qq.com>
2019-05-08 11:19:40 +0800

    fa_tools: Able to install toolchain

--- fa_tools/build.sh
+++ fa_tools/build.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/bin/bash -u
 
-LINUX_PLAT="LINUX platform"
-ANDROID_PLAT="ANDROID platform"
 function pt_error()
 {
     echo -e "\033[1;31mERROR: $*\033[0m"
@@ -17,14 +15,17 @@ function pt_info()
     echo -e "\033[1;32mINFO: $*\033[0m"
 }
 
-function execute_cmd() 
+function run_cmd() 
 {
     eval $@ || exit $?
 }
 
 function usage()
 {
-    echo -e "\033[1;32mUsage: `basename $0` -b board[${H5_BOARD}] -p platform[${H5_PLATFORM}] -t target[${H5_TARGET}]\033[0m"
+    echo -e "usage: $0 -b board -p platform -t target"
+    echo -e "\t -b ${FA_BOARD}"
+    echo -e "\t -p ${FA_PLATFORM}"
+    echo -e "\t -t ${FA_TARGET}"
     exit 1
 }
 
@@ -33,55 +34,57 @@ function parse_arg()
     if [ $# -lt 4 ]; then
         usage
     fi
+
+    local found=0
     OLD_IFS=${IFS}
     IFS="|"
     while getopts "b:p:t:" opt
     do
         case $opt in
             b )
-                BOARD_NAME=$OPTARG
-                for TMP in ${H5_BOARD}
+                SELECTED_BOARD=$OPTARG
+                for tmp in ${FA_BOARD}
                 do
-                    if [ ${BOARD_NAME} = ${TMP} ];then
-                        FOUND=1
+                    if [ ${SELECTED_BOARD} = ${tmp} ];then
+                        found=1
                         break
                     else
-                        FOUND=0
+                        found=0
                     fi
                 done
-                if [ ${FOUND} -eq 0 ]; then
+                if [ ${found} -eq 0 ]; then
                     pt_error "unsupported board"
                     usage
                 fi
                 ;;
             p )
-                PLATFORM=$OPTARG
-                for TMP in ${H5_PLATFORM}
+                SELECTED_PLATFORM=$OPTARG
+                for tmp in ${FA_PLATFORM}
                 do
-                    if [ ${PLATFORM} = ${TMP} ];then
-                        FOUND=1
+                    if [ ${SELECTED_PLATFORM} = ${tmp} ];then
+                        found=1
                         break
                     else
-                        FOUND=0
+                        found=0
                     fi
                 done
-                if [ ${FOUND} -eq 0 ]; then
+                if [ ${found} -eq 0 ]; then
                     pt_error "unsupported platform"
                     usage
                 fi
                 ;;
             t )
-                TARGET=$OPTARG
-                for TMP in ${H5_TARGET}
+                SELECTED_TARGET=$OPTARG
+                for tmp in ${FA_TARGET}
                 do
-                    if [ ${TARGET} = ${TMP} ];then
-                        FOUND=1
+                    if [ ${SELECTED_TARGET} = ${tmp} ];then
+                        found=1
                         break
                     else
-                        FOUND=0
+                        found=0
                     fi
                 done
-                if [ ${FOUND} -eq 0 ]; then
+                if [ ${found} -eq 0 ]; then
                     pt_error "unsupported target"
                     usage
                 fi
@@ -96,38 +99,48 @@ function parse_arg()
 function pack_lichee_4_linux()
 {
     cd ${PRJ_ROOT_DIR}
-    execute_cmd "./build.sh pack" 
+    run_cmd "./build.sh pack" 
+
+    (cd ./${AW_KER}/output/lib/modules/ && tar czf ${FA_OUT}/3.4.39.tar.gz 3.4.39-h3)
+    run_cmd "cp ./tools/pack/out/boot0_sdcard.fex ${FA_OUT}/"
+    run_cmd "cp ./tools/pack/out/u-boot.fex ${FA_OUT}/"
+    run_cmd "cp ./tools/pack/out/boot.fex ${FA_OUT}/boot.img"
+
+    tree ${FA_OUT}
 }
 
 function build_uboot_4_linux()
 {
     cd ${PRJ_ROOT_DIR}
-    execute_cmd "./build.sh -p sun8iw7p1 -b nanopi-h3 -m uboot"
+    run_cmd "./build.sh -p sun8iw7p1 -b nanopi-h3 -m uboot"
     pack_lichee_4_linux
-    pt_info "build and pack u-boot for ${LINUX_PLAT} success"    
+    pt_info "Build and pack u-boot for ${LINUX_PLAT_MSG} success"    
 }
 
 function  build_kernel_4_linux()
 {
     cd ${PRJ_ROOT_DIR}
-    execute_cmd "./build.sh -p sun8iw7p1 -b nanopi-h3 -m kernel"
+    run_cmd "./build.sh -p sun8iw7p1 -b nanopi-h3 -m kernel"
     pack_lichee_4_linux
-    pt_info "build and pack kernel for ${LINUX_PLAT} success"
+
+    pt_info "Build and pack kernel for ${LINUX_PLAT_MSG} success"
 }
 
 function  build_lichee_4_linux()
 {
     cd ${PRJ_ROOT_DIR}
-    execute_cmd "./build.sh -p sun8iw7p1 -b nanopi-h3"
+    run_cmd "./build.sh -p sun8iw7p1 -b nanopi-h3"
     pack_lichee_4_linux
-    pt_info "build and pack lichee for ${LINUX_PLAT} success"
+
+    pt_info "Build and pack lichee for ${LINUX_PLAT_MSG} success"
 }
 
 function build_clean_4_linux()
 {
     cd ${PRJ_ROOT_DIR}
-    execute_cmd "./build.sh -p sun8iw7p1_linux -b nanopi-h3 -m clean"
-    pt_info "clean lichee for ${LINUX_PLAT} success"
+    run_cmd "./build.sh -p sun8iw7p1_linux -b nanopi-h3 -m clean"
+    run_cmd "rm ./fa_out -rf"
+    pt_info "Clean lichee for ${LINUX_PLAT_MSG} success"
 }
 
 function pack_lichee_4_android()
@@ -143,89 +156,115 @@ function pack_lichee_4_android()
 function build_uboot_4_android()
 {
     cd ${PRJ_ROOT_DIR}
-    execute_cmd "./build.sh -p sun8iw7p1_android -b nanopi-h3 -m uboot"
+    run_cmd "./build.sh -p sun8iw7p1_android -b nanopi-h3 -m uboot"
     pack_lichee_4_android
-    pt_info "build and pack u-boot for ${ANDROID_PLAT} success"    
+    pt_info "Build and pack u-boot for ${ANDROID_PLAT_MSG} success"    
 }
 
 function  build_kernel_4_android()
 {
     cd ${PRJ_ROOT_DIR}
-    execute_cmd "./build.sh -p sun8iw7p1_android -b nanopi-h3 -m kernel"
+    run_cmd "./build.sh -p sun8iw7p1_android -b nanopi-h3 -m kernel"
     pack_lichee_4_android
-    pt_info "build and pack kernel for ${ANDROID_PLAT} success"
+    pt_info "Build and pack kernel for ${ANDROID_PLAT_MSG} success"
 }
 
 function build_lichee_4_android()
 {
     cd ${PRJ_ROOT_DIR}
-    execute_cmd "echo -e \"2\n\" | ./build.sh lunch ${BOARD_NAME}"
-    pt_info "build lichee for ${ANDROID_PLAT} success. Please build and pack in Android directory"
+    run_cmd "echo -e \"2\n\" | ./build.sh lunch ${SELECTED_BOARD}"
+    pt_info "build lichee for ${ANDROID_PLAT_MSG} success. Please build and pack in Android directory"
 }
 
 function build_clean_4_android()
 {
     cd ${PRJ_ROOT_DIR}
-    execute_cmd "./build.sh -p sun8iw7p1_android -b nanopi-h3 -m clean"
-    pt_info "clean lichee for ${ANDROID_PLAT} success"
+    run_cmd "./build.sh -p sun8iw7p1_android -b nanopi-h3 -m clean"
+    pt_info "clean lichee for ${ANDROID_PLAT_MSG} success"
 }
 
 function update_config()
 {
     rm -rf ./.config
-    echo "BOARD_NAME=${BOARD_NAME}" >>.config
-    echo "PLATFORM=${PLATFORM}" >>.config
-    echo "TARGET=${TARGET}" >>.config
+    echo "BOARD_NAME=${SELECTED_BOARD}" >>.config
+    echo "PLATFORM=${SELECTED_PLATFORM}" >>.config
+    echo "TARGET=${SELECTED_TARGET}" >>.config
 }
 
+function prepare_toolchain()
+{
+    local src_dir=${PRJ_ROOT_DIR}/fa_tools/toolchain_h3_linux-3.4
+    local target_dir=${PRJ_ROOT_DIR}/brandy/toolchain
+
+    if [ ! -e ${target_dir}/gcc-linaro-arm.tar.xz ]; then
+        [ -d ${src_dir} ] || git clone https://github.com/friendlyarm/toolchain_h3_linux-3.4 ${src_dir} --depth 1 -b master
+        run_cmd "cat ${src_dir}/gcc-linaro-arm.tar.xz* >${target_dir}/gcc-linaro-arm.tar.xz"
+    fi
+}
+
+DEBUG="no"
 cd ..
 PRJ_ROOT_DIR=`pwd`
-H5_BOARD="nanopi-air|nanopi-neo|nanopi-m1|nanopi-m1-plus|nanopi-duo2"
-H5_PLATFORM="linux|android"
-H5_TARGET="all|u-boot|kernel|pack|clean"
-BOARD_NAME=none
-PLATFORM=linux
-TARGET=all
+AW_KER=linux-3.4
+
+LINUX_PLAT_MSG="Linux platform"
+ANDROID_PLAT_MSG="Android platform"
+
+# friendlyelec attribute
+FA_BOARD="nanopi-air|nanopi-neo|nanopi-m1|nanopi-m1-plus"
+FA_PLATFORM="linux|android"
+FA_TARGET="all|u-boot|kernel|pack|clean"
+FA_OUT=${PRJ_ROOT_DIR}/fa_out
+
+# user attribute
+SELECTED_BOARD="unset"
+SELECTED_PLATFORM="unset"
+SELECTED_TARGET="unset"
 SYS_CONFIG_DIR=${PRJ_ROOT_DIR}/tools/pack/chips/sun8iw7p1/configs/nanopi-h3
 
 parse_arg $@
 update_config
 
-pt_info "preparing sys_config.fex"
 cd ${PRJ_ROOT_DIR}
-cp -rvf ${SYS_CONFIG_DIR}/board/sys_config_${BOARD_NAME}.fex ${SYS_CONFIG_DIR}/sys_config.fex
+mkdir -p ${FA_OUT}
+
+pt_info "preparing sys_config.fex"
+cp -rvf ${SYS_CONFIG_DIR}/board/sys_config_${SELECTED_BOARD}.fex ${SYS_CONFIG_DIR}/sys_config.fex
 touch ./linux-3.4/.scmversion
 
-if [ "x${PLATFORM}" = "xlinux" ]; then
-    if [ "x${TARGET}" = "xpack" ]; then
+prepare_toolchain
+if [ "x${SELECTED_PLATFORM}" = "xlinux" ]; then
+    if [ "x${SELECTED_TARGET}" = "xpack" ]; then
         pack_lichee_4_linux
-        pt_info "pack lichee for ${LINUX_PLAT} success"   
-    elif [ "x${TARGET}" = "xu-boot" ]; then
+        pt_info "Pack lichee for ${LINUX_PLAT_MSG} success"   
+    elif [ "x${SELECTED_TARGET}" = "xu-boot" ]; then
         build_uboot_4_linux
-    elif [ "x${TARGET}" = "xkernel" ]; then
+    elif [ "x${SELECTED_TARGET}" = "xkernel" ]; then
         build_kernel_4_linux
-    elif [ "x${TARGET}" = "xall" ]; then
+    elif [ "x${SELECTED_TARGET}" = "xall" ]; then
         build_lichee_4_linux
-    elif [ "x${TARGET}" = "xclean" ]; then
+    elif [ "x${SELECTED_TARGET}" = "xclean" ]; then
         build_clean_4_linux
     else
-        pt_error "unsupported target"
+        pt_error "unsupported target: ${SELECTED_TARGET}"
         usage
     fi
-elif [ "x${PLATFORM}" = "xandroid" ]; then
-    if [ "x${TARGET}" = "xpack" ]; then
+elif [ "x${SELECTED_PLATFORM}" = "xandroid" ]; then
+    if [ "x${SELECTED_TARGET}" = "xpack" ]; then
         pack_lichee_4_android
-        pt_info "pack lichee for ${ANDROID_PLAT} success" 
-    elif [ "x${TARGET}" = "xu-boot" ]; then
+        pt_info "Pack lichee for ${ANDROID_PLAT_MSG} success" 
+    elif [ "x${SELECTED_TARGET}" = "xu-boot" ]; then
         build_uboot_4_android
-    elif [ "x${TARGET}" = "xkernel" ]; then
+    elif [ "x${SELECTED_TARGET}" = "xkernel" ]; then
         build_kernel_4_android
-    elif [ "x${TARGET}" = "xall" ]; then
+    elif [ "x${SELECTED_TARGET}" = "xall" ]; then
         build_lichee_4_android
-    elif [ "x${TARGET}" = "xclean" ]; then
+    elif [ "x${SELECTED_TARGET}" = "xclean" ]; then
         build_clean_4_android
     else
-        pt_error "unsupported target"
+        pt_error "unsupported target: ${SELECTED_TARGET}"
         usage
     fi
+else
+    pt_error "unsupported platform: ${SELECTED_PLATFORM}"
 fi
